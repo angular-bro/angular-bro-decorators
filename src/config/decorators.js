ff5278e5f728c9b2e387faac7877328747ac2216
Adam Bullmer <psycodrumfreak@gmail.com>
2017-07-17 00:21:47 -0500

    Added state, config, and run decorators

--- src/config/decorators.js
+++ src/config/decorators.js
@@ -5,9 +5,9 @@ export default decoratorsModule;
 
 let $injector;
 
-decoratorsModule.run(_$injector_ => {
-  $injector = _$injector_;
-});
+decoratorsModule.run(['$injector', (_$injector_) => {
+    $injector = _$injector_;
+}]);
 
 /**
  * @example
@@ -30,23 +30,23 @@ decoratorsModule.run(_$injector_ => {
  *  }
  */
 export function inject (...components) {
-  if (typeof components[0] === 'object') {
-    let key = components[1];
+    if (typeof components[0] === 'object') {
+        let key = components[1];
 
-    return {
-        get: () => {
-          try {
-            return $injector.get(key);
-          } catch (err) {
-            throw new Error(`${key} cannot be injected as a property. Inject it in the controller.`);
-          }
-        }
-      };
-  } else {
-    return function decorate (target, key, property) {
-      target.$inject = components;
-    };
-  }
+        return {
+            get: () => {
+                try {
+                    return $injector.get(key);
+                } catch (err) {
+                    throw new Error(`${key} cannot be injected as a property. Inject it in the controller.`);
+                }
+            }
+        };
+    } else {
+        return function decorate (target/*, key, property*/) {
+            target.$inject = components;
+        };
+    }
 }
 
 /**
@@ -61,17 +61,17 @@ export function inject (...components) {
  *  }
  */
 export function injectAs (dep) {
-  return function decorate (target, key, descriptor) {
-    return {
-        get: () => {
-          try {
-            return $injector.get(dep);
-          } catch (err) {
-            throw new Error(`${name} cannot be injected as a property. Inject it in the controller.`);
-          }
-        }
-      };
-  };
+    return function decorate (/*target, key, descriptor*/) {
+        return {
+            get: () => {
+                try {
+                    return $injector.get(dep);
+                } catch (err) {
+                    throw new Error(`${name} cannot be injected as a property. Inject it in the controller.`);
+                }
+            }
+        };
+    };
 }
 
 /**
@@ -99,40 +99,40 @@ export function injectAs (dep) {
  *  }
  */
 export function directive (opts) {
-  return function decorate (Target) {
-    let name = opts.name || getTargetName(Target);
-    name = name.substring(0,1).toLowerCase() + name.substring(1);
-    function factory(...deps) {
-      let inject = Target.$inject || [];
-      let directiveDefinitionObject = {
-        priority: opts.priority,
-        template: opts.template,
-        templateUrl: opts.templateUrl,
-        transclude: opts.transclude,
-        restrict: opts.restrict,
-        templateNamespace: opts.templateNamespace,
-        scope: opts.scope,
-        controller: [...inject, function (...deps) {
-          return new Target(...deps);
-        }],
-        controllerAs: opts.controllerAs || 'ctrl',
-        bindToController: opts.bindToController || true,
-        require: opts.require
-      };
-      if (Target.compile) {
-        directiveDefinitionObject.compile = function compile(...args) {
-          return Target.compile(...args);
-        };
-      }
-      if (Target.link) {
-        directiveDefinitionObject.link = function link(...args) {
-          return Target.link(...args);
-        };
-      }
-      return directiveDefinitionObject;
-    }
-    decoratorsModule.directive(name, factory);
-  };
+    return function decorate (Target) {
+        let name = opts.name || getTargetName(Target);
+        name = name.substring(0,1).toLowerCase() + name.substring(1);
+        function factory(/*...deps*/) {
+            let inject = Target.$inject || [];
+            let directiveDefinitionObject = {
+                priority: opts.priority,
+                template: opts.template,
+                templateUrl: opts.templateUrl,
+                transclude: opts.transclude,
+                restrict: opts.restrict,
+                templateNamespace: opts.templateNamespace,
+                scope: opts.scope,
+                controller: [...inject, function (...deps) {
+                    return new Target(...deps);
+                }],
+                controllerAs: opts.controllerAs || 'controller',
+                bindToController: opts.bindToController || true,
+                require: opts.require
+            };
+            if (Target.compile) {
+                directiveDefinitionObject.compile = function compile(...args) {
+                    return Target.compile(...args);
+                };
+            }
+            if (Target.link) {
+                directiveDefinitionObject.link = function link(...args) {
+                    return Target.link(...args);
+                };
+            }
+            return directiveDefinitionObject;
+        }
+        decoratorsModule.directive(name, factory);
+    };
 }
 
 
@@ -146,14 +146,14 @@ export function directive (opts) {
  *  export default class MyController {}
  */
 export function register (opts) {
-  return function decorate(target) {
-    if(opts.inject) {
-      target.$inject = opts.inject;
-    }
+    return function decorate(target) {
+        if(opts.inject) {
+            target.$inject = opts.inject;
+        }
 
-    let name = opts.name || getTargetName(target);
-    decoratorsModule[opts.type](name, target);
-  };
+        let name = opts.name || getTargetName(target);
+        decoratorsModule[opts.type](name, target);
+    };
 }
 
 /**
@@ -164,7 +164,7 @@ export function register (opts) {
  *  export default class MyController {}
  */
 export function controller (target) {
-  return register({ type: 'controller' })(target);
+    return register({ type: 'controller' })(target);
 }
 /**
  * @example
@@ -182,13 +182,13 @@ export function controller (target) {
  *  }
  */
 export function filter (Target) {
-  let name = getTargetName(Target);
-  name = name.substring(0,1).toLowerCase() + name.substring(1);
-  let deps = Target.$inject || [];
-  decoratorsModule.filter(name, [...deps, function (...deps) {
-    let instance = new Target();
-    return function (...args) { return instance.filter(...args); };
-  }]);
+    let name = getTargetName(Target);
+    name = name.substring(0,1).toLowerCase() + name.substring(1);
+    let deps = Target.$inject || [];
+    decoratorsModule.filter(name, [...deps, function (/*...deps*/) {
+        let instance = new Target();
+        return function (...args) { return instance.filter(...args); };
+    }]);
 }
 /**
  * @example
@@ -202,9 +202,9 @@ export function filter (Target) {
  *  }
  */
 export function constant (Target) {
-  let name = getTargetName(Target);
-  name = name.substring(0,1).toLowerCase() + name.substring(1);
-  return register({ type: 'constant', name: name })(new Target());
+    let name = getTargetName(Target);
+    name = name.substring(0,1).toLowerCase() + name.substring(1);
+    return register({ type: 'constant', name: name })(new Target());
 }
 /**
  * @example
@@ -218,7 +218,7 @@ export function constant (Target) {
  *  }
  */
 export function value (Target) {
-  return register({ type: 'value', name: getTargetName(Target) })(new Target());
+    return register({ type: 'value', name: getTargetName(Target) })(new Target());
 }
 /**
  * @example
@@ -228,7 +228,7 @@ export function value (Target) {
  *  export default class MyFactory {}
  */
 export function factory (target) {
-  return register({ type: 'factory' })(target);
+    return register({ type: 'factory' })(target);
 }
 /**
  * @example
@@ -238,7 +238,7 @@ export function factory (target) {
  *  export default class MyService {}
  */
 export function service (target) {
-  return register({ type: 'service' })(target);
+    return register({ type: 'service' })(target);
 }
 /**
  * @example
@@ -248,7 +248,82 @@ export function service (target) {
  *  export default class Myprovider {}
  */
 export function provider (target) {
-  return register({ type: 'provider' })(target);
+    return register({ type: 'provider' })(target);
+}
+
+/**
+ * Angular UI Router State Factory Decorator
+ *
+ * Same import rules apply as the directives and such above.
+ *
+ * @example
+ *  // All the examples below are equivalent.
+ *  import {state} from './decorators';
+ *
+ *  @state('index', {
+ *    // default options
+ *    url: '/index',
+ *    templateUrl: 'index/template.html',
+ *    constrollerAs: 'controller',
+ *    // additional options...
+ *  })
+ *  export default class IndexController {}
+ *
+ *
+ *  @state('index')
+ *  export default class IndexController {}
+ *
+ *
+ *  @state('index')()
+ */
+export function state (name, opts) {
+    return function decorate (Target) {
+        function factory($stateProvider) {
+            let stateDefinitionObject = Object.assign({
+                url: `/${name}`,
+                templateUrl: `${name}/template.html`,
+                controllerAs: 'controller',
+            }, opts);
+
+            if (typeof Target === 'function') {
+                let inject = Target.$inject || [];
+                stateDefinitionObject.controller = [...inject, function (...deps) {
+                    return new Target(...deps);
+                }];
+            }
+
+            $stateProvider.state(name, stateDefinitionObject);
+        }
+        decoratorsModule.config(['$stateProvider', factory]);
+    };
+}
+
+/**
+ * @example
+ *  import {config} from './decorators'
+ *
+ *  @config
+ *  export default class MyConfig {}
+ */
+export function config (Target) {
+    let deps = Target.$inject || [];
+    decoratorsModule.config([...deps, function (...deps) {
+        new Target(...deps);
+    }]);
+}
+
+/**
+ * @example
+ *  import {run} from './decorators'
+ *
+ *  @run
+ *  export default class MyRun {}
+ */
+export function run (Target) {
+    let deps = Target.$inject || [];
+    decoratorsModule.run([...deps, function (...deps) {
+        new Target(...deps);
+    }]);
 }
 
 
@@ -256,9 +331,9 @@ export function provider (target) {
  * Polyfill for IE to return Target.name
  */
 function getTargetName (o) {
-  if (o.name) {
-    return o.name;
-  }
-  // if IE
-  return o.toString().match(/function\s?(.*)\s?\(/)[1];
+    if (o.name) {
+        return o.name;
+    }
+    // if IE
+    return o.toString().match(/function\s?(.*)\s?\(/)[1];
 }
